3c68ffcb3fbd3225896c7b1d11e064f25bac13a7
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2025-06-01 23:54:04 -0300

    Início da classe curso

--- src/model/dao/CursoDAO.java
+++ src/model/dao/CursoDAO.java
@@ -1,5 +1,66 @@
 package model.dao;
 
-public class CursoDAO {
+import br.edu.fateczl.Lista;
+import br.edu.fateczl.pilha.Pilha;
+import model.dto.Curso;
+import util.CsvUtils;
+
+import java.io.IOException;
+
+public class CursoDAO implements IGenericDAO<Curso, String> {
+
+	public final String caminhoArquivo = "C:\\TEMP\\cursos.csv";
+
+	@Override
+	public void salvar(Curso curso) throws Exception {
+
+		CsvUtils.adicionarLinhaCSV(caminhoArquivo, toCSV(curso));
+
+	}
+
+	@Override
+	public Pilha<Curso> buscarTodos() throws Exception {
+
+		Lista<String[]> dados = CsvUtils.lerArquivo(caminhoArquivo);
+		Pilha<Curso> cursos = new Pilha<>();
+
+		for (int i = 0, lenght = dados.size(); i < lenght; i++) {
+
+			Integer id = Integer.parseInt(dados.get(i)[0]);
+			String nome = dados.get(i)[1];
+			Integer idAreaConhecimento = Integer.parseInt(dados.get(i)[1]);
+
+			Curso curso = new Curso(id, nome, idAreaConhecimento);
+
+			cursos.push(curso);
+
+		}
+
+		return cursos;
+	}
+
+	@Override
+	public void atualizar(Curso entidade) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void excluir(String id) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Curso buscarPorID(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toCSV(Curso entidade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/model/dto/Curso.java
+++ src/model/dto/Curso.java
@@ -5,5 +5,41 @@ public class Curso {
 	String nome;
 	Integer idAreaConhecimento;
 
-	
+	public Curso(Integer id, String nome, Integer idAreaConhecimento) {
+
+		this.id = id;
+		this.nome = nome;
+		this.idAreaConhecimento = idAreaConhecimento;
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdAreaConhecimento() {
+		return idAreaConhecimento;
+	}
+
+	public void setIdAreaConhecimento(Integer idAreaConhecimento) {
+		this.idAreaConhecimento = idAreaConhecimento;
+	}
+
+	@Override
+	public String toString() {
+		return "Curso [id=" + id + ", nome=" + nome + ", idAreaConhecimento=" + idAreaConhecimento + "]";
+	}
+
 }
