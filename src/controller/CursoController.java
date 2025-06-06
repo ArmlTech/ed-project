c515f1ecc4d6214c3276fa0619ff9b1ba95b7049
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2025-06-05 23:09:31 -0300

    Atualizações antes da CursoView

--- src/controller/CursoController.java
+++ src/controller/CursoController.java
@@ -1,5 +1,88 @@
 package controller;
 
-public class CursoController {
+import br.edu.fateczl.Lista;
+import br.edu.fateczl.pilha.Pilha;
+import model.dto.Curso;
+import service.AreaConhecimentoService;
+import service.CursoService;
+
+public class CursoController implements IGenericController<Curso, Integer> {
+
+	private final CursoService service;
+	private final AreaConhecimentoService areaService;
+
+	public CursoController() {
+
+		this.service = new CursoService();
+		this.areaService = new AreaConhecimentoService();
+
+	}
+
+	@Override
+	public void salvar(Curso entidade) throws Exception {
+
+		service.salvar(entidade);
+
+	}
+
+	@Override
+	public Pilha<Curso> buscarTodos() throws Exception {
+		return service.buscarTodos();
+	}
+
+	@Override
+	public void atualizar(Curso entidade) throws Exception {
+
+		service.atualizar(entidade);
+
+	}
+
+	@Override
+	public void excluir(Curso entidade) throws Exception {
+
+		service.excluir(entidade.getId());
+
+	}
+
+	@Override
+	public Curso buscarPorID(Integer id) throws Exception {
+
+		return service.buscarPorID(id);
+
+	}
+
+	@Override
+	public Curso criarEntidade(Curso entidade, Lista<String> dadosInput) throws Exception {
+
+		for (int i = 0, lenght = dadosInput.size(); i < lenght; i++) {
+
+			if (dadosInput.get(i).isBlank()) {
+
+				throw new Exception("Preencha todos os campos");
+
+			}
+
+		}
+
+		Integer id;
+
+		if (entidade == null) {
+
+			id = 0;
+
+		} else {
+
+			id = entidade.getId();
+
+		}
+
+		Integer idCurso = Integer.parseInt(dadosInput.get(0));
+		String nome = dadosInput.get(1);
+		Integer idAreaConhecimento = Integer.parseInt(dadosInput.get(2));
+
+		Curso curso = new Curso(idCurso, nome, idAreaConhecimento);
+		return curso;
+
+	}
 
 }
